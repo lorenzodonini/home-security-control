2c2db27a3a78f2f22fb45e25946f59ce0a91fc8d
Lorenzo Donini <lorenzo.donini@motius.de>
2017-10-16 13:48:42 +0200

    Add AlarmToggleButton custom view

---
+++ android-app/app/src/main/java/com/donini/tech/homesec/view/AlarmToggleButton.kt
@@ -0,0 +1,37 @@
+package com.donini.tech.homesec.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.RelativeLayout
+import com.donini.tech.homesec.R
+import kotlinx.android.synthetic.main.alarm_toggle_button.view.*
+
+class AlarmToggleButton @JvmOverloads constructor(context: Context,
+                                                  attrs: AttributeSet,
+                                                  defStyleAttr: Int = 0,
+                                                  defStyleRes: Int = 0): RelativeLayout(context, attrs, defStyleAttr, defStyleRes) {
+    enum class State(val backgroundResource: Int) {
+        ON(R.drawable.toggle_on),
+        PENDING(R.drawable.toggle_pending),
+        OFF(R.drawable.toggle_off)
+    }
+
+    var state = State.OFF
+    set(value) {
+        setBackgroundResource(value.backgroundResource)
+    }
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.alarm_toggle_button, this, true)
+    }
+
+    fun setClickListener(onClickListener: (view: View) -> Unit) {
+        alarmButton.setOnClickListener { v -> onClickListener(v) }
+    }
+
+    fun setClickListener(onClickListener: OnClickListener) {
+        alarmButton.setOnClickListener(onClickListener)
+    }
+}
